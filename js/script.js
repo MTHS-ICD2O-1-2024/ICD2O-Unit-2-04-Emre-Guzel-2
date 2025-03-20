60168144ef7565d450ca393d1734bb56b114c5f1
default <yunus.emre.guzel@mths.ca>
2025-03-20 17:12:58 +0000

    added js calcualtins

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Emre Guzel
-// Created on: March 18  2025
+// Created on: March 20  2025
 // This file contains the JS functions for index.html
 
-function streetAddress() {
+"use strict"
+
+function calculatoinOfAreaOtriangle() {
   // Setting the varibles
-  const streetNumber = document.getElementById("street-number").value
-  const streetName = document.getElementById("street-name").value
-  document.getElementById("address").innerHTML ="Your address is: " +  streetNumber + " " + streetName + "."
+  const baseOfTheTriangle = parseFloat(document.getElementById("base-of-the-triangle").value)
+  const heightOfTheTriangle = parseFloat (document.getElementById("height-of-the-triangle").value)
+  const calculatoinOfAreaOtriangle = (baseOfTheTriangle * heightOfTheTriangle) / 2 
+  // clacualtoins 
+  document.getElementById("area").innerHTML ="Area is: " + calculatoinOfAreaOtriangle + "cm²"
 }
