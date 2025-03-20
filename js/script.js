7bc96e282bd085578096df86a041bc82ea3d120c
default <yunus.emre.guzel@mths.ca>
2025-03-20 16:55:29 +0000

    added teplete

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Emre Guzel
+// Created on: March 18  2025
 // This file contains the JS functions for index.html
 
-/**
- * Displays an alert with the message "Hello, World!"
- */
-function myButtonClicked() {
-  alert("Hello, World!")
+function streetAddress() {
+  // Setting the varibles
+  const streetNumber = document.getElementById("street-number").value
+  const streetName = document.getElementById("street-name").value
+  document.getElementById("address").innerHTML ="Your address is: " +  streetNumber + " " + streetName + "."
 }
